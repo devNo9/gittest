1e4c73bf4e91c49acf4203172c2113288da14449
user2 <user2@DESKTOP-3F82LPB>
2021-04-27 20:37:14 +0900

    #08 test2매소드추가 -bs-

--- gittest/src/com/git/controller/GitController.java
+++ gittest/src/com/git/controller/GitController.java
@@ -5,4 +5,10 @@ public class GitController {
 	public void test() {
 		System.out.println("추가한클래스 매소드");
 	}
+	
+	
+	public void test2() {
+		System.out.println("원격으로 저장하자!");
+	}
+	
 }
--- gittest/src/com/git/model/service/GitService.java
+++ gittest/src/com/git/model/service/GitService.java
@@ -1,9 +1,7 @@
 package com.git.model.service;
 
 public class GitService {
-
-	public void searchName123() {
-	}
+	
 	public void checkTest() {
 
 		
